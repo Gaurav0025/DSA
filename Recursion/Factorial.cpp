b9319f8a330877898c80fabd0cba052ff315fd5e
Gaurav kumar <gkarora889@gmail.com>
2024-02-06 02:08:09 +0530

    comment

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-// bsdk 
-//  hello 
+// using recursion
 int factorial(int n){
     if(n == 0 || n ==1) return 1;
     return n*factorial(n-1);
