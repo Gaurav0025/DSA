7eaba2b63a6d6773a695b4502b019da41361b7b6
Gaurav kumar <gkarora889@gmail.com>
2024-02-08 19:00:26 +0530

    first offcampus interview,questions are asked in test.cpp file

---
+++ clg/test.cpp
@@ -0,0 +1,41 @@
+
+// 1. Write a function to check if a given number is a prime number
+
+// #include<iostream>
+// using namespace std;
+// void checkPrime( int a){
+//     for(int i=2;i<a;i++ ){
+//         if(a % i == 0){
+//             cout<<"Not Prime";
+//             break;
+//         }
+//         i++;
+//         cout<<"Prime";
+//     }   
+// }
+// int main(){
+//     int a;
+//     cin>>a;
+//     checkPrime(a);
+// }
+
+
+
+// 2. Write a function to compute the the sum of n even numbers
+
+// #include<iostream>
+// using namespace std;
+// int sumNumber( int a){
+//     int count = 0;
+//     for(int i =0;i<=a;i++){
+//         count = count + i;
+//         i++;
+//     }
+//     return count;
+// }
+// int main(){
+//     int a;
+//     cin>>a;
+//     int result = sumNumber( a);
+//     cout<< result;
+// }
